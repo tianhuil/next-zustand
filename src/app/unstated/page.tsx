ecbee4e3291df2fcca1345d591b8bada1f7dbf28
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-05-05 21:49:01 -0400

    Tweak counters

--- src/app/unstated/page.tsx
+++ src/app/unstated/page.tsx
@@ -5,17 +5,22 @@ import { createContainer } from 'unstated-next'
 let views = 0
 
 const CounterState = createContainer(() => {
-    const [counter, setCounter] = React.useState(0)
-    const increment = () => setCounter(counter + 1)
+    const [counter, setCounter] = React.useState<{count: number}>({count: 0})
+    const increment = () => setCounter({count: counter.count + 1})
     return {counter, increment}
 })
 
+const OtherComponent: React.FC = () => {
+    views++
+    return <div>OtherComponent Views: {views}</div>
+}
+
 const _Page: React.FC = () => {
-  views++
   const {counter, increment} = CounterState.useContainer()
   return <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div>Unstated Counter: {counter} Views: {views}</div>
+    <div>Unstated Counter: {counter.count} </div>
     <button onClick={increment}>Increment</button>
+    <OtherComponent/>
   </main>
 }
 
--- src/app/zustand/page.tsx
+++ src/app/zustand/page.tsx
@@ -8,12 +8,17 @@ const useCounter = create<{counter: number, increment: () => void}>((set) => ({
 
 let views = 0
 
-const Zustand: React.FC = () => {
+const OtherComponent: React.FC = () => {
   views++
+  return <div>OtherComponent Views: {views}</div>
+}
+
+const Zustand: React.FC = () => {
   const {counter, increment} = useCounter()
   return <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div>Zustand Counter: {counter} Views: {views}</div>
     <button onClick={increment}>Increment</button>
+    <OtherComponent/>
   </main>
 }
 
